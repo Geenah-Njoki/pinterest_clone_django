4200a799ae3301150f28539790823c599fd7d87a
Geenah-Njoki <georginanjoki7@gmail.com>
2021-10-08 15:17:58 -0700

    Added a form

--- landing/views.py
+++ landing/views.py
@@ -1,13 +1,26 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import RandomForm
+
+from landing.forms import RandomForm
 
 # Create your views here.
 
+
 def home(request):
 
+    fruits=["mango", "apple", "melon", "avocado"] 
+
+
     context = {
-        "title" : "My first title"
+        "title" : "My first title",
+        "form" : RandomForm(),
+        "first_name" : "Rey",
+        "last_name" : "Mysterio",
+        "second_name" : "Best",
+        "third_name" : "Wrestler",
+        "fruits" : fruits,
     }
 
     return render(request, 'home.html', context)
--- pinterest_clone/settings.py
+++ pinterest_clone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5%m0!15c)k&xzacyqog3)eib&(yx*)8f_^yxlc0fv7)z$i-a%e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.100.183", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.100.183", "127.0.0.1", "192.168.0.14"]
 
 
 # Application definition
