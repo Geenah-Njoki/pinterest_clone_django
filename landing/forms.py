effbf052098650b14ac5e0c75ea01873370304c1
Geenah-Njoki <georginanjoki7@gmail.com>
2021-10-15 14:23:49 -0700

    Added user_nav

--- landing/forms.py
+++ landing/forms.py
@@ -5,7 +5,7 @@ class RandomForm(forms.Form):
     number = forms.IntegerField()
 
 class RegisterForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username                                       '}))
     email = forms.EmailField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
     age = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Age'}))
